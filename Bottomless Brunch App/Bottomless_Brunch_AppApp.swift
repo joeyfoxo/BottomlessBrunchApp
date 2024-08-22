5e6f9580103aad1714dddb9d74dc84a25c7aa8f6
Joey <james.lawrence@live.co.uk>
2024-08-22 22:22:46 +0100

    Initial Commit

---
+++ Bottomless Brunch App/Bottomless_Brunch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bottomless_Brunch_AppApp.swift
+//  Bottomless Brunch App
+//
+//  Created by Joey on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Bottomless_Brunch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
